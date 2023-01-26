2adc00c6a62d2b406db7b2f7964a4fb2f96db54b
Juansu01 <thejuank0@hotmail.com>
2023-01-26 11:21:14 -0500

    Update: Modifying reducer so that items are displayed with their respective amount instead of displaying repeated items

--- ReactMeals/src/components/Cart/Cart.js
+++ ReactMeals/src/components/Cart/Cart.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from './Cart.module.css';
 import Modal from "../UI/Modal";
+import CartContext from "../store/cart-context";
+import CartItem from './CartItem'
 
 function Cart(props) {
 
+    const cartContext = useContext(CartContext);
+
+    const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+    const hasItems = cartContext.items.length > 0;
+
+    function cartItemRemoveHandler(itemId) {
+
+    }
+
+    function cartItemAddHandler(item) {
+
+    }
+
     const cartItems = (
         <ul className={styles['cart-items']}>
-            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-                <li key={item.id}>{item.name}</li>
+            {cartContext.items.map((item) => (
+                <CartItem 
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             ))}
         </ul>
     )
@@ -17,11 +39,11 @@ function Cart(props) {
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button onClick={props.onClickHideCart} className={styles['button--alt']}>Close</button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     )
